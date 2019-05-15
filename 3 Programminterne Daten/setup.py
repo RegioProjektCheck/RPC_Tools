7a9d522496114cc5794dced84abdd6ee23290187
ChrFr <christoph.franke@web.de>
2019-05-15 17:52:15 +0200

    v1.0 wheel

--- 3 Programminterne Daten/setup.py
+++ 3 Programminterne Daten/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rpctools",
-    version="1.0",
+    version="1.0.0",
 
     packages=find_packages('.', exclude=['ez_setup']),
     #namespace_packages=['rpctools'],
